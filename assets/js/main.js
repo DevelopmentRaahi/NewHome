1104f4973e7e0ecff98927b60f0e7c3f7ac52196
developertool45 <developertool45@gmail.com>
2024-08-01 14:11:47 +0530

    header

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,11 @@
 
 (function() {
   "use strict";
+
+  
+  document.addEventListener('scroll', function () {
+    console.log(document.documentElement.scrollTop);
+  })
   let year = document.querySelector('.year')
   let py = (new Date()).getFullYear()
   year.textContent = py;
@@ -10,8 +15,10 @@
   function toggleScrolled() {
     const selectBody = document.querySelector('body');
     const selectHeader = document.querySelector('#header');
+    
     if (!selectHeader.classList.contains('scroll-up-sticky') && !selectHeader.classList.contains('sticky-top') && !selectHeader.classList.contains('fixed-top')) return;
     window.scrollY > 100 ? selectBody.classList.add('scrolled') : selectBody.classList.remove('scrolled');
+   
   }
 
   document.addEventListener('scroll', toggleScrolled);
