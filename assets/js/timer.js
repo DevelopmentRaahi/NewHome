fc865f7ab53a60989470d28caa7bedb62556c040
developertool45 <developertool45@gmail.com>
2024-09-06 10:17:36 +0530

    updated

--- assets/js/timer.js
+++ assets/js/timer.js
@@ -6,29 +6,29 @@ function startCountdown(minutes, seconds) {
     const mins = Math.floor(remainingTime / 60);
     const secs = remainingTime % 60;
 
-    // Format minutes and seconds
+   
     const formattedMinutes = mins < 10 ? "0" + mins : mins;
     const formattedSeconds = secs < 10 ? "0" + secs : secs;
 
-    // Update the timer display
+ 
     timerDisplay.querySelector("#minutes").textContent = formattedMinutes;
     timerDisplay.querySelector("#seconds").textContent = formattedSeconds;
 
-    // Stop the timer when it reaches zero
+   
     if (remainingTime <= 0) {
       clearInterval(countdownInterval);
-      closePopup(); // Optionally hide the pop-up
+      closePopup(); 
     }
 
-    remainingTime--; // Decrease the remaining time by 1 second
-  }, 1000); // Run every second
+    remainingTime--; 
+  }, 1000); 
 }
 
 function closePopup() {
   document.getElementById("discountPopup").style.display = "none";
 }
 
-// Start the countdown when the page loads
+
 window.onload = function () {
-  startCountdown(15, 45); // Start the countdown from 15:45
+  startCountdown(15, 45); 
 };
